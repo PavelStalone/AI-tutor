f89cd54d2a263ddbd7c67473de58bb66c83cc149
Pavel Shoplik <pavel.shoplik@ansiblehealth.com>
2025-02-25 23:21:28 +0300

    Tool calling test

--- src/main/kotlin/rut/uvp/family/FamilyApplication.kt
+++ src/main/kotlin/rut/uvp/family/FamilyApplication.kt
@@ -1,11 +1,32 @@
 package rut.uvp.family
 
+import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.prompt.Prompt
+import org.springframework.ai.ollama.OllamaChatModel
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Description
+import java.util.function.Function
 
 @SpringBootApplication
-class FamilyApplication
+class FamilyApplication {
+
+    @Bean
+    fun runner(chatClient: ChatClient) = CommandLineRunner { _ ->
+        println("Start question")
+
+        chatClient
+            .prompt("Какая температура в Москве?")
+            .tools(WeatherTools())
+            .call()
+            .content().also {
+                println(it)
+            }
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<FamilyApplication>(*args)
+    runApplication<FamilyApplication>(*args)
 }
---
+++ src/main/kotlin/rut/uvp/family/OllamaConfig.kt
@@ -0,0 +1,50 @@
+package rut.uvp.family
+
+import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.model.ChatModel
+import org.springframework.ai.ollama.OllamaChatModel
+import org.springframework.ai.ollama.api.OllamaApi
+import org.springframework.ai.ollama.api.OllamaOptions
+import org.springframework.ai.tool.annotation.Tool
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableConfigurationProperties(OllamaConfig.OllamaClientProperties::class)
+class OllamaConfig {
+
+    @ConfigurationProperties(prefix = "spring.ai.ollama")
+    data class OllamaClientProperties(
+        val baseUrl: String,
+        val model: String
+    )
+
+    @Bean
+    fun chatModel(properties: OllamaClientProperties): ChatModel {
+        return OllamaChatModel.builder()
+            .ollamaApi(OllamaApi(properties.baseUrl))
+            .defaultOptions(
+                OllamaOptions.builder()
+                    .model(properties.model)
+                    .temperature(0.4)
+                    .build()
+            )
+            .build()
+    }
+
+    @Bean
+    fun chatClient(chatModel: ChatModel): ChatClient {
+        return ChatClient.create(chatModel)
+    }
+}
+
+class WeatherTools {
+
+    @Tool(description = "Получить температуру по местположению")
+    fun currentWeather(location: String): String {
+        println("currentWeather called: $location")
+        return "25"
+    }
+}
